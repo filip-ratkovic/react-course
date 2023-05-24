d120502a73611b77e66c433bbbc1f76ef97a4834
Filip <filip.ratkovic.fr@gmail.com>
2023-05-24 19:56:38 +0200

    score

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
-import ToDo from './Projects/ToDo/ToDo'
-import Score from './Projects/Score/Score'
 
 export default class App extends Component {
   
   render() {
     return (
       <div className="app">
-        <Score/>
       </div>
     )
   }
--- src/Projects/Score/Score.jsx
+++ src/Projects/Score/Score.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// import React, { useState } from 'react'
 
-function Score() {
-  return (
-    <div>Score</div>
-  )
-}
+// function Score() {
+//   const [teams, setTeams] = useState()
+//   const [teamName, setTeamName] = useState()
 
-export default Score
+//   const newTeam = []
+//   const addTeam = () => {
+//     newTeam.push(...newTeam, teamName)
+//     setTeams(newTeam)
+//   }
+//   console.log(teams)
+//   return (
+//     <main className="score-main">
+//         <input type="text" onChange={((e) => {
+//             setTeamName(e.target.value);
+//         })} />
+//         <button onClick={addTeam}></button>
+
+//         {teams.map((team)=> {
+//          return <h1>{team}</h1>
+//         })}
+//     </main>
+//     )
+// }
+
+// export default Score
